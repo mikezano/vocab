bcc1b927c31c26b68af8e23f14e0fb6595f616aa
mikezano <mikezano@gmail.com>
2020-12-04 11:33:47 -0700

    instructions

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+# Vocab
 
-`dotnet watch run`
-- Gets pseudo live-reloading, when you refresh brower a build happens
+A .NET Blazor WebAssembly project that allows you to create a set of multiple choice flashcards based off of a configured google sheet.
 
-- On the first time,  you have to trust the certificate
-`dotnet dev-certs https --trust`
+## Instructions
+
+To create your own deck of cards make sure you publish to the web a google sheet. Instruction on publishing can be found here
+
+Once published take note of the highlighted portion of the image below. This is the **google sheet id**
+
+Refer to the image below on how to structure your google sheet. Notice the highlighted region indicating the **google sheet id** This will be used in the initial form to indicate where your
+
+![](Web/wwwroot/images/sheet-id.png)
---
+++ Web/Api/GoogleSheet.cs
@@ -0,0 +1,63 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Web.Models;
+
+
+
+namespace Web.Api
+{
+    public class GoogleSheetCell
+    {
+        public string CellName { get; set; }
+        public string CellValue { get; set; }
+    }
+
+    public class GoogleSheet
+    {
+        private static string BASE_URL = "https://spreadsheets.google.com/feeds/cells/{0}/od6/public/basic?alt=json";
+        private readonly HttpClient client;
+
+        public GoogleSheet(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<List<TranslationItem>> GetEntries(string sheetId)
+        {
+            string url = String.Format(BASE_URL, sheetId);
+            var entries = await client.GetFromJsonAsync<object>(url);
+            var translations = FormatWords(entries);
+            return translations;
+        }
+
+        public List<TranslationItem> FormatWords(object json)
+        {
+            JObject o = JObject.Parse(json.ToString());
+
+            List<GoogleSheetCell> cells =
+                o.SelectToken("feed.entry")
+                .Select(s => new GoogleSheetCell
+                {
+                    CellName = (string)s.SelectToken("title.$t"),
+                    CellValue = (string)s.SelectToken("content.$t")
+                }).ToList();
+
+
+            var columnA = cells.Where(w => w.CellName.StartsWith('A') && w.CellName != "A1").Select(s => s.CellValue).ToList();
+            var columnB = cells.Where(w => w.CellName.StartsWith('B') && w.CellName != "B1").Select(s => s.CellValue).ToList();
+
+            var translations = new List<TranslationItem>();
+            for(int i=0; i < columnA.Count; i++)
+            {
+                translations.Add(new TranslationItem { Spanish = columnA[i], English = columnB[i] });
+            }
+            return translations;
+        }
+    }
+}
